e7792bc3dac497b64f1fecfdc05bb3d2d1a6a474
Gebrehiwot (G-Nahom) <gebrunayom@gmail.com>
2022-01-03 17:02:59 +0300

    get data from remote server using axios completed

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,67 +1,125 @@
-import {useRef,useState} from 'react';
-import Navbar from './Navbar'
-const Form=()=>{
-    const borderStyle={
-        border:'3px solid steelblue',
-        boxShadow:'3px green inset'
-    }
+import React,{Component} from  'react';
+import {Button} from 'primereact/button';
+import {Card} from 'primereact/card'
+import  axios  from 'axios';
+import { InputText } from 'primereact/inputtext';
+import {DataTable} from 'primereact/datatable'
+import {Column} from 'primereact/column'
+import {Calendar} from 'primereact/calendar'
 
-    const centerButton={
-        display:'block',
-        margin:'auto',
-        
-    }
-    const [data,setData]=useState({});
-    const  firstName=useRef();
-    const  password = useRef();
-    const  email = useRef();
-    const handleSubmit=(data,event)=>{
-        event.preventDefault();
-       const tempdata={
-            fname:firstName.current.value,
-            email:email.current.value,
-            password:password.current.value
+class Form extends Component{
+
+    constructor(){
+        super();
+        this.state={
+           
+                Deivice_Name:'',
+                Device_Model:'',
+                Production_Year:'',
+                IMEIL:'',
+                Produced_By:''
+            ,
+            data:[]
         }
-       // this.setData(tempdata);
-       console.log(tempdata);
-       alert('insered data')
        
     }
-    return(
+    
+    componentDidMount(){
+       axios.get('http://localhost:8000/deviceapi').then(
+           (res)=>{
+                this.setState({data:res.data})
+               console.log('responese',this.state.data);
+           }
+       ).catch('Error')
+    }
+    handleSubmit=(e)=>{
+        e.preventDefault();
+        const formData={
+            Deivice_Name:this.state.Deivice_Name,
+            Device_Model:this.state.Device_Model,
+            Production_Year:this.state.Production_Year,
+            IMEIL:this.state.IMEIL,
+            Produced_By:this.state.Produced_By
+        }
+        const header=  {
+            'Access-Control-Allow-Origin' : '*',
+            'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+            }
+        axios.post( 'http://localhost:8000/deviceapi',
+            formData,{header}
+           
+          ).then((res)=>console.log(res)).catch('Error');
+       // axios.post('http://localhost:8000/deviceapi',{formData}).then((res)=>console.log(res)).catch('Eroor')
+        console.log('submitted..',formData);
+    }
+    
+    render=()=>{
+        const marginStyle={
+            marginBottom:'10px',
+            marginTop:'10px'
+        }  
+        const inputStyle={
+            width:'50%',
+            display:'block',
+            margin:'auto'
+        }
+        const paginatorLeft = <Button type="button" icon="pi pi-refresh" className="p-button-text" />;
+        const paginatorRight = <Button type="button" icon="pi pi-cloud" className="p-button-text" />;
         
-        <div className="jubotron">
-            <Navbar/>
-        <div  className="row">
-            <div className="col"></div>
-
-         <div  style={borderStyle} className="col card shadow">
-         <form  onSubmit={handleSubmit}>
-             <div className="card-title text-center"><h1>SIGN UP</h1></div>
-             <div className="form-group">
-            
-             <input ref={firstName}  type="text" placeholder="First Name" className="form-control"/>
+        return(
+            <div className='container rounded bg-secondary mt-5'>
+                <h2 className='text-center shadow rounded bg-warning text-danger'>Electronic Device Data <i className='pi pi-user'></i> </h2>
+              <div className='d-flex justify-content-center'>
+              <Card style={{width:'70%'}}>
+             <div>
+             <form>
+              <div style={marginStyle} className="">
+                <i className="pi" />
+                <InputText style={inputStyle}  value={this.state.Deivice_Name}  onChange={(e) => this.setState({Deivice_Name:e.target.value})} placeholder="Device Name" />
             </div>
             
-            <br/>
-
-             <div className="form-group">
-             <input ref={email}  type="text" placeholder="Emial" className="form-control"/>
- 
+            <div>
+            <span style={marginStyle}>
+                <i className="" />
+                <InputText style={inputStyle} value={this.state.Device_Model}  onChange={(e) => this.setState({Device_Model:e.target.value} )} placeholder="Device Model" />
+            </span>
+            
+            </div>
+            <div style={inputStyle}>
+            <Calendar style={{marginTop:'10px',width:'100%'}} placeholder='Production Date' value={this.state.Production_Year} dateFormat="dd/mm/yy"  onChange={(e) => this.setState({Production_Year:e.target.value})}/>
+            </div>
+            <div style={marginStyle} className=''>
+                <i className=''/>
+                <InputText  style={inputStyle} value={this.state.IMEIL} type='number'  onChange={(e)=>this.setState({IMEIL:e.target.value})}  placeholder='IMEIL'/>
+            </div>
+            <div style={marginStyle} className=''>
+                <i className=''/>
+                <InputText  style={inputStyle} value={this.state.Produced_By} type='text'  onChange={(e)=>this.setState({Produced_By:e.target.value})}  placeholder='Produced By'/>
+            </div>
+            
+            <br/>        
+                <Button onClick={this.handleSubmit} className='p-button-info'style={{display:'block',margin:'auto'}} label="Send"><i className='pi pi-send'></i></Button>
+            </form>   
              </div>
-             <br/>
-             <div className="form-group">
-
-             <input ref={password} name="password" type="password" placeholder="Password" className="form-control"/>
-              
+              </Card>
+              </div>
+            <div className='card'>
+            <DataTable value={this.state.data}
+            paginator responsiveLayout="scroll"
+            paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
+            currentPageReportTemplate="Showing {first} to {last} of {totalRecords}" rows={2} rowsPerPageOptions={[10,20,50]}
+            paginatorLeft={paginatorLeft} paginatorRight={paginatorRight}
+            >
+              <Column field='Deivice_Name' header='Deivice_Name' sortable/>
+              <Column field='Device_Model' header='Device_Model'/>
+              <Column field='Produced_By' header='Produced_By'/>
+              <Column field='Production_Year' header='Production_Year'/>
+             
+              </DataTable>
+                </div>
              </div>
-             <button style={centerButton} className="btn btn-success text-ceter">Submit</button>
-        <br/>
-         </form>
-         </div>
-         <div className="col"></div>
-        </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Form;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,34 @@
+import React, { Component } from 'react';
+import {Sidebar} from 'primereact/sidebar'
+import { Button } from 'primereact/button';
+import { Card } from 'primereact/card' 
+import {Link} from 'react-router-dom';
+class Navbar extends Component{
 
-import {Link, NavLink} from 'react-router-dom'
- function Navbar(){
-      return(
-        <div className='shadow'>
-               <div className='row bg-dark'>
-               <div className='col'></div>
-               <div className='col d-flex flex-row'>
-                   <Link className='p-3 text-light' to='/home'>Home</Link>
-                   <Link className='p-3 text-light' to='/todo'>Todo</Link>
-                   <Link className='p-3 text-light' to='/form'>Form</Link>
-               </div>
-               </div>
-            </div>
-      )
+  constructor(props){
+    super(props);
+    this.state={
+       ifvisible:true
+    }
+  }
+
+  render(){
+    return(
+      <>
+      <Sidebar onHide={()=>this.setState({ifvisible:!this.state.ifvisible})} visible={this.state.ifvisible}>
+
+       <Card>
+
+       </Card>
+      </Sidebar>
+      <div>
+        {this.state.ifvisible?'':<Button onClick={()=>this.setState({ifvisible:!this.state.ifvisible})}><i className='pi pi-list'></i></Button>
+      }
+      </div>
+      </>
+    )
+  }
+
 }
 
 export default Navbar;
